ea0797e2803fb0cf4ffe803d19ab5dd7d8cbd34e
jacob975 <z123a123s123@gmail.com>
2018-04-03 17:03:38 +0800

    Management code of py path

--- DL_conf.py
+++ DL_conf.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 '''
 Program:
 This is a table of environment variable of deep learning python.
@@ -21,21 +21,9 @@ update log
 # python path is where you install python
 # tensorflow python can run under both python3 and python2
 # please check the path by $which python python2
-path_of_python = "/usr/bin/python"
-
-#--------------- Setting data path ----------------
-# data path means where will you put your row image.
-# recommand: /home/username
-path_of_data = "/home/Jacob975/bin/deep_learning/20180104_exercise/ysodata"
+path_of_python = "/usr/bin/python3"
 
 #--------------- Setting source code path ------------------
 # code path means where do you install these code about tat.
 # recommand: /home/username/bin/tat_python
-path_of_source_code = "/home/Jacob975/bin/deep_learning/20180104_exercise"
-
-#--------------- Setting result path ----------------
-# result path means once you produce some data by tat_python.
-# result path will be where to save.
-# recommand: /home/tat_result
-path_of_result = "/home/Jacob975/bin/deep_learning/20180104_exercise/result"
-
+path_of_source_code = "/home/Jacob975/bin/deep_learning"
---
+++ DL_setup.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+'''
+Program:
+This is a program to setup tat python3 code. 
+For Linux and macOS only
+Usage:
+    1. tat_setup.py
+editor:
+    Jacob975
+
+##################################
+#   Python3                      #
+#   This code is made in python3 #
+##################################
+
+20170809
+#################################
+update log
+20180403 version alpha 1:
+    1. the code to modify python3 path.
+
+'''
+import time
+import glob
+import os
+import fnmatch
+import DL_conf
+from sys import platform, exit
+
+def readfile(filename):
+    f = open(filename, 'r')
+    data = []
+    for line in f.readlines():
+        # skip if no data or it's a hint.
+        if line == "\n" or line.startswith('#'):
+            continue
+        data.append(line[:-1])
+    f.close
+    return data
+
+# This method is used to set the path in the first line of programm
+def set_path_linux(py_path, path = ""):
+    py_list = glob.glob("{0}/*.py".format(path))
+    for name in py_list:
+        temp = 'sed -i "1s?.*?{0}?" {1}'.format(py_path, name)
+        if VERBOSE>0:print (temp)
+        os.system(temp)
+
+# This method is used to set the path in the first line of each tat_python programm file
+def set_path_mac(py_path, path = ""):
+    py_list = glob.glob("{0}/*.py".format(path))
+    for name in py_list:
+        temp = 'sed -i '' -e "1s?.*?{0}?" {1}'.format(py_path, name)
+        if VERBOSE>0:print (temp)
+        os.system(temp)
+    # remove by-product
+    temp = "rm *-e"
+    os.system(temp)
+
+#--------------------------------------------
+# main code
+VERBOSE = 1
+# measure times
+start_time = time.time()
+# get the path of python from DL_conf.py
+py_path = DL_conf.path_of_python
+py_path = "#!{0}".format(py_path)
+if VERBOSE>0:
+    print ("path of python3 writen in DL_conf.py: {0}".format(py_path))
+# get path of DL code from DL_conf
+code_path = DL_conf.path_of_source_code
+# set path of python into all tat_python program file
+if platform == "linux" or platform == "linux2":
+    set_path_linux(py_path, code_path)
+    # process all code in nest folder
+    obj_list = glob.glob("*")
+    for obj in obj_list:
+        if os.path.isdir(obj):
+            os.chdir(obj)
+            temp_path = "{0}/{1}".format(code_path, obj)
+            set_path_linux(py_path, temp_path)
+            os.chdir(code_path)
+
+elif platform == "darwin":
+    set_path_mac(py_path, code_path)
+    # process all code in nest folder
+    obj_list = glob.glob("*")
+    for obj in obj_list:
+        if os.path.isdir(obj):
+            os.chdir(obj)
+            temp_path = "{0}/{1}".format(code_path, obj)
+            set_path_mac(py_path, temp_path)
+            os.chdir(code_path)
+
+else:
+    print ("you system is not fit the requirement of tat_python")
+    print ("Please use Linux of macOS")
+    exit()
+
+# back to path of code
+os.chdir(code_path)
+# measuring time
+elapsed_time = time.time() - start_time
+print ("Exiting Setup Program, spending ", elapsed_time, "seconds.")
