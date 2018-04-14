ad19f9f312e069fdb93668fa37750c31dacb7f9b
jacob975 <z123a123s123@gmail.com>
2018-04-14 21:13:27 +0800

    Minor fix

--- test_schedule.sh
+++ test_schedule.sh
@@ -8,22 +8,28 @@
 # The code work
 
 # check arguments
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ]; then
     echo "Illegal number of parameters"
-    echo "Usage: ${0} [the folder name saved AI]"
+    echo "Usage: ${0} [DIR where save AI] [key word]"
     exit 1
 fi
 
 # Array of AI name
-array=(${2}*/)
-
+AI_POOL=${1}
+keyword=${2}
 echo "AI saved directory going to test:"
-for each in "${array[@]}";
+for each in ${AI_POOL}/2018*/;
 do
-    echo "${each}"
-    mkdir "AI_test_${each}"
-    sed_test_AI_64_8.py source_sed_MaxLoss16.npy source_id_MaxLoss16.npy "AI_test_${each}"\
-                        "${each}checkpoint_AI_64_8_source_sed_MaxLoss16" \
-                        > "AI_test_${each}/result_of_AI_test"
+    # ${each##*/} means only take the last word of $each
+    # ${each::-1} means take $each but the last latter. 
+    FULL_AI_NAME=${each::-1}
+    AI_NAME=${FULL_AI_NAME##*/}
+    echo "${AI_NAME}"
+
+    # create a directory to save result of testing
+    mkdir "AI_${AI_NAME}_test_on_${keyword}"
+    sed_test_AI_64_8.py source_sed_${keyword}.npy source_id_${keyword}.npy "AI_${AI_NAME}_test_on_${keyword}"\
+                        "${each}checkpoint_AI_64_8_source_sed_MaxLoss15" \
+                        > "AI_${AI_NAME}_test_on_${keyword}/result_of_AI_test"
 done
 exit 0
